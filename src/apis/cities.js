dfced8c8dec13f0327a899abb922625bd59e0f17
Nickson Munene Njeru <nicknionene7@gmail.com>
2023-10-16 15:14:39 +0300

    Add all code

---
+++ src/apis/cities.js
@@ -0,0 +1,190 @@
+export const cities = [
+  {
+    id: "1",
+    county_name: "Mombasa",
+  },
+  {
+    id: "2",
+    county_name: "Kwale",
+  },
+  {
+    id: "3",
+    county_name: "Kilifi",
+  },
+  {
+    id: "4",
+    county_name: "Tana River",
+  },
+  {
+    id: "5",
+    county_name: "Lamu",
+  },
+  {
+    id: "6",
+    county_name: "Taita Makueni",
+  },
+  {
+    id: "7",
+    county_name: "Garissa",
+  },
+  {
+    id: "8",
+    county_name: "Wajir",
+  },
+  {
+    id: "9",
+    county_name: "Mandera",
+  },
+  {
+    id: "10",
+    county_name: "Marsabit",
+  },
+  {
+    id: "11",
+    county_name: "Isiolo",
+  },
+  {
+    id: "12",
+    county_name: "Meru",
+  },
+  {
+    id: "13",
+    county_name: "Tharaka-Nithi",
+  },
+  {
+    id: "14",
+    county_name: "Embu",
+  },
+  {
+    id: "15",
+    county_name: "Kitui",
+  },
+  {
+    id: "16",
+    county_name: "Machakos",
+  },
+  {
+    id: "17",
+    county_name: "Makueni",
+  },
+  {
+    id: "18",
+    county_name: "Nyandarua",
+  },
+  {
+    id: "19",
+    county_name: "Nyeri",
+  },
+  {
+    id: "20",
+    county_name: "Kirinyaga",
+  },
+  {
+    id: "21",
+    county_name: "Murang'a",
+  },
+  {
+    id: "22",
+    county_name: "Kiambu",
+  },
+  {
+    id: "23",
+    county_name: "Turkana",
+  },
+  {
+    id: "24",
+    county_name: "West Pokot",
+  },
+  {
+    id: "25",
+    county_name: "Samburu",
+  },
+  {
+    id: "26",
+    county_name: "Trans-Nzoia",
+  },
+  {
+    id: "27",
+    county_name: "Uasin Gishu",
+  },
+  {
+    id: "28",
+    county_name: "Elgeyo-Marakwet",
+  },
+  {
+    id: "29",
+    county_name: "Nandi",
+  },
+  {
+    id: "30",
+    county_name: "Baringo",
+  },
+  {
+    id: "31",
+    county_name: "Laikipia",
+  },
+  {
+    id: "32",
+    county_name: "Nakuru",
+  },
+  {
+    id: "33",
+    county_name: "Narok",
+  },
+  {
+    id: "34",
+    county_name: "Kajiado",
+  },
+  {
+    id: "35",
+    county_name: "Kericho",
+  },
+  {
+    id: "36",
+    county_name: "Bomet",
+  },
+  {
+    id: "37",
+    county_name: "Kakamega",
+  },
+  {
+    id: "38",
+    county_name: "Vihiga",
+  },
+  {
+    id: "39",
+    county_name: "Bungoma",
+  },
+  {
+    id: "40",
+    county_name: "Busia",
+  },
+  {
+    id: "41",
+    county_name: "Siaya",
+  },
+  {
+    id: "42",
+    county_name: "Kisumu",
+  },
+  {
+    id: "43",
+    county_name: "Homa Bay",
+  },
+  {
+    id: "44",
+    county_name: "Migori",
+  },
+  {
+    id: "45",
+    county_name: "Kisii",
+  },
+  {
+    id: "46",
+    county_name: "Nyamira",
+  },
+  {
+    id: "47",
+    county_name: "Nairobi",
+  },
+];
